007941c0e38e18cf5fa07dbb00d24bfaa03f0996
Joe2k <jonathansamuel2k@gmail.com>
2021-02-02 13:06:20 +0530

    Google Auth

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, {useState, useContext} from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/Home";
+import Login from "./src/Login";
+import AuthContext from "./src/auth/context";
 
 export default function App() {
+  const [user, setuser] = useState();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <AuthContext.Provider value={{ user, setuser }}>
+          <View style={styles.container}>
+              {user ? <Home /> : <Login />}
+              <StatusBar style="auto" />
+          </View>
+      </AuthContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
